e62b3879d3d1f76fbc2ee4c25d4e2d440032be79
furqanzshaikh <furqanshaikh329@gmail.com>
2024-09-08 13:20:43 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -6,11 +6,22 @@ const twilio = require('twilio');
 require("dotenv").config();
 const bodyParser = require('body-parser');
 const app = express();
+const { VoiceResponse } = require('twilio').twiml;
+const axios = require('axios');
 
-// Middleware
+axios.post('http://localhost:3000/gather-response', 'Digits=1', {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
+.then(response => {
+  console.log('Response:', response.data);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
 app.use(express.json());
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
@@ -21,23 +32,17 @@ app.get("/", (req, res) => {
   res.send("Working");
 });
 
-
-const VoiceResponse = twilio.twiml.VoiceResponse;
-
-
-
-
 const accountSid = process.env.ACCOUNT_SID; 
 const authToken = process.env.AUTH_TOKEN; 
 const client = twilio(accountSid, authToken);
 const yourPhoneNumber = process.env.PHONE_NO; 
-const newNum = process.env.NEW_NUM
+const newNum = process.env.NEW_NUM;
 
-// Twilio Call to Phone Number
+
 const initiateCall = () => {
   client.calls
     .create({
-      url: 'https://9f7e1234.ngrok.io/twiml', 
+      url: 'https://7228-2401-4900-52fc-9490-e07f-da7a-a2c2-cdc8.ngrok-free.app/twiml', // Updated with your ngrok URL
       to: newNum,
       from: yourPhoneNumber,
     })
@@ -45,6 +50,30 @@ const initiateCall = () => {
     .catch(error => console.error(error));
 };
 
+app.post('/initiate-call', (req, res) => {
+  initiateCall();
+  res.send('Call initiated');
+});
+app.post('/initiate-call', (req, res) => {
+  initiateCall();
+  res.send('Call initiated');
+});
+
+// Endpoint to Serve Twilio IVR
+app.post('/twiml', (req, res) => {
+  console.log(req.body)
+  const twiml = new VoiceResponse();
+  twiml.say('Hello, press 1 to receive your interview link.');
+  
+  twiml.gather({
+    numDigits: 1,
+    action: '/gather-response',
+    method: 'POST',
+  });
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
 // Endpoint to Serve Twilio IVR
 app.post('/twiml', (req, res) => {
   const twiml = new VoiceResponse();
@@ -60,7 +89,16 @@ app.post('/twiml', (req, res) => {
   res.type('text/xml');
   res.send(twiml.toString());
 });
-
+const sendInterviewLink = () => {
+  client.messages
+    .create({
+      body: 'Here is your personalized interview link: https://your-interview-link.com',
+      from: yourPhoneNumber,
+      to: newNum
+    })
+    .then(message => console.log(`Message SID: ${message.sid}`))
+    .catch(error => console.error(error));
+};
 // Endpoint to Handle Button Press
 app.post('/gather-response', (req, res) => {
   const buttonPressed = req.body.Digits;
@@ -78,17 +116,6 @@ app.post('/gather-response', (req, res) => {
 });
 
 // Function to Send Interview Link via SMS
-const sendInterviewLink = () => {
-  client.messages
-    .create({
-      body: 'Here is your personalized interview link: https://your-interview-link.com',
-      from: yourPhoneNumber,
-      to: newNum
-    })
-    .then(message => console.log(`Message SID: ${message.sid}`))
-    .catch(error => console.error(error));
-};
-
 
 
 // Start server
